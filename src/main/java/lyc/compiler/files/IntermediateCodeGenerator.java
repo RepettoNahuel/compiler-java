158f90e3d032f493c768a0d91f36c487104918a3
Nahuel Repetto <repettonahuelbautista@gmail.com>
2025-10-15 22:41:12 -0300

    Commit Parcial - Corrección en impresión de tercetos

--- src/main/java/lyc/compiler/files/IntermediateCodeGenerator.java
+++ src/main/java/lyc/compiler/files/IntermediateCodeGenerator.java
@@ -2,40 +2,30 @@ package lyc.compiler.files;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class IntermediateCodeGenerator implements FileGenerator {
 
-    private static final List<String[]> tercetos = new ArrayList<>();
-    private static int tempCount = 0;
+    private static final Map<Integer, String[]> tercetosMap = new HashMap<>();
+    private static int tercNumber = 0;
 
-    public static String newTemp() {
-        return "T" + (tempCount++);
-    }
-
-    public static void addTerceto(String op, Object arg1, Object arg2) {
-        if(arg1 != null)
-            arg1 = arg1.toString();
-        if(arg2 != null)
-            arg2 = arg2.toString();
-        tercetos.add(new String[]{op, (String)arg1, (String)arg2});
-    }
-
-    public static void addTerceto(String op, Object arg1, Object arg2, Object result) {
-        if(arg1 != null)
-            arg1 = arg1.toString();
-        if(arg2 != null)
-            arg2 = arg2.toString();
-        if(result != null)
-            result = result.toString();
-        tercetos.add(new String[]{op, (String)arg1, (String)arg2, (String)result});
+    public static String addTerceto(String op, Object arg1, Object arg2) {
+        String sArg1 = arg1 != null ? arg1.toString() : null;
+        String sArg2 = arg2 != null ? arg2.toString() : null;
+        Integer key = tercNumber;
+        tercetosMap.put(key, new String[]{op, sArg1, sArg2});
+        tercNumber++;
+        return "[" + key.toString() + "]";
     }
 
     @Override
     public void generate(FileWriter fileWriter) throws IOException {
-        for(String[] t : tercetos) {
-            fileWriter.write(String.join(", ", t) + "\n");
+        for (Map.Entry<Integer, String[]> entry : tercetosMap.entrySet()) {
+            Integer key = entry.getKey();
+            String[] t = entry.getValue();
+            String line = String.format("[%d] = (%s, %s, %s)\n", key, t[0], t[1], t[2]);
+            fileWriter.write(line);
         }
     }
 }
